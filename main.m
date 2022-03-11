0e11a9ac4f6324e147f02c958b9ef94e94d5733a
Arexh <644564286@qq.com>
2022-03-11 13:37:51 +0800

    rename logs

--- main.m
+++ main.m
@@ -58,7 +58,7 @@ addpath('./ThirdParty/WorkerObjWrapper');
 %% Init variables
 CurrentSummary = Summary( ...
 ...
-    'PSO-5(15+3)-median', ... % LogPathName
+    'mQSO-5(15+3)-median', ... % LogPathName
     'main.log', ... % DFileName
     100, ... % EnvironmentNumber
     31, ... % RunNumber
